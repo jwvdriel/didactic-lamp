6ec0aafafc0ac3e5a5aa59822871dbb18b4fe9a2
Jan-Willem <jan-willem@pop-os.localdomain>
2022-12-27 15:51:11 +0100

    added route

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ def home_page():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/api')
+def api():
+    with open('data.json', mode='r') as my_file:
+        text = my_file.read()
+        return text
