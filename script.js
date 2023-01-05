027738e49bbff24d93961902129ed8c57f7cd2f3
Jan-Willem <jan-willem@pop-os.localdomain>
2023-01-05 16:22:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let cardNameInput = document.querySelector('#input-name-text')
+let cardNumberInput = document.querySelector('#card-number-input');
+let cardInputMonth = document.querySelector('#month-input');
+let cardInputYear = document.querySelector('#year-input');
+let cardVerification = document.querySelector('#verication-input')
+
+let submit = document.querySelector('#button-submit')
+
+
+if (cardNameInput.value !== '' && cardNameInput.value !== null) {
+    let newName = document.querySelector('#paragaph')
+    console.log(newName)
+
+}
+
